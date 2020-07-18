deafabda5f2debed6d19362abd25127355c07cb4
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-18 14:00:50 -0700

    Beta 5.py
    An archive of the beta 5 edition of MEDOS 2, in Python format.

---
+++ Old/MEDOS 2/Beta 5.py
@@ -0,0 +1,223 @@
+# CodeSkulptor runs Python programs in your browser.
+# Click the upper left button to run this simple demo.
+
+# CodeSkulptor is tested to run in recent versions of
+# Chrome, Firefox, and Safari.
+
+# Start the frame animation
+terminaloop = int
+terminaloop == 1
+print("MEDOS 2.0 Beta")
+print("Developer edition")
+print("Copyleft () Sean Walla Walla")
+print("<<<=<<=<>=>>=>>>")
+startupByUser = input("Press Enter to start: ")
+print (" ")
+while terminaloop == 1:
+    consolein = input("PY://")
+    if consolein == ("help"):
+        print("Command help")
+        print("GUI - Opens the GUI interface")
+        print("PYVER - shows the python version")
+        print("Quit - ends the session")
+    elif consolein == ("GUI"):
+        frame.start()
+    elif consolein == ("PYVER"):
+        print("Python version: 3.6")
+    elif consolein == ("Quit"):
+        terminaloop = 0
+	else:
+		print ("Cannot perform task. Try checking your syntax")
+print("Shutting down")
+print("Please wait")
+# message = "Welcome to MEDOS!"
+'''
+
+# Handler for mouse click
+def pyDrive():
+    global message
+    message = ("Contents of PY://")
+
+
+def commandLis():
+    global message
+    message = ("Commands")
+    global message2
+
+
+def disettings():
+    global message
+    message = ("Display settings")
+    global message2
+    message2 = ("Font color: [GREEN] ")
+    global message3
+    message3 = ("Font size: [20]")
+    global message4
+    message4 = ("Resolution: [480x360]")
+    global message5
+    message5 = ("========")
+
+
+def faresetbut():
+    global message
+    message = ("Are you sure you want to perform a factory reset?")
+    print("Welcome to the MEDOS installation module")
+    startReset1 = input("Press [ENTER] key to start installation")
+    print("\n\n\n\n\n\n\n\nMEDOS installer")
+    print("Set up partitions")
+    print("File System: [GPT]")
+    print("Total space: 256 GB")
+    print("How many partitions?")
+    print("1x   | 256 GB each")
+    print("2x   | 128 GB each")
+    print("4x   | 64 GB each")
+    print("8x   | 32 GB each")
+    print("16x  | 16 GB each")
+    print("32x  | 8 GB each")
+    print("64x  | 4 GB each")
+    print("128x | 2 GB each")
+    print("256x | 1 GB each")
+    partcountconfirm = int(input("How many partitions do you need?"))
+    if partcountconfirm == 1:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 1 complete")
+    if partcountconfirm == 2:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 2 complete")
+    if partcountconfirm == 4:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 4 complete")
+    if partcountconfirm == 8:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 8 complete")
+    if partcountconfirm == 16:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 16 complete")
+    if partcountconfirm == 32:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 32 complete")
+    if partcountconfirm == 64:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 64 complete")
+    if partcountconfirm == 128:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 128 complete")
+    if partcountconfirm == 256:
+        print("Partitioning 256 GB")
+        print("Please wait, this may take a while")
+        print("0 of 256 complete")
+    print("Partitioning complete")
+    startReset2 = input("Press [ENTER] key to continue installation")
+    print("\n\n\n\n\n\n\n\nSetup network connection")
+    print("MEDOS is a wireless file system for external storage devices")
+    print("You can choose to use MEDOS remotely and set up settings here, or you can continue and set up later")
+    print("Connection type: [Wi-Fi] [1x] [2x] [3G] [4G] [5G] [Wi-Fi Direct] [Bluetooth]")
+    networkcontype = str(input("Enter the connection type you chose from above"))
+    if networkcontype == ("Wi-Fi"):
+        print("Connection setup as Wi-Fi")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("1x"):
+        print("Connection setup as 1x cellular service")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("2x"):
+        print("Connection setup as 2x cellular service")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("3G"):
+        print("Connection setup as 3G cellular service")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("4G"):
+        print("Connection setup as 4G cellular service")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("5G"):
+        print("Connection setup as 5G cellular service")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("Wi-Fi direct"):
+        print("Connection setup as Wi-Fi Direct")
+        networktype = str
+        networktype = (networkcontype)
+    if networkcontype == ("BlueTooth"):
+        print("Connection setup as Peer to Peer BlueTooth")
+        networktype = str
+        networktype = (networkcontype)
+        print("Network setup complete")
+        startReset3 = input("Press [ENTER] key to continue installation")
+        print("\n\n\n\n\n\n\n\nUser account setup")
+        print("Create an account for this device")
+        print("User 1 (owner)")
+        username1 = str(input("Username: "))
+        password1 = input("Password: ")
+        print("User 2 (guest)")
+        username2 = str(input("Username: "))
+        password2 = input("Password: ")
+        print("User setup complete!")
+        startReset4 = input("Press [ENTER] key to continue installation")
+        print("\n\n\n\n\n\n\n\nStarting")
+        print("Please wait as we get your device ready")
+        print("Installing...")
+        installed = input("Installation complete!")
+        print("Press [ENTER] key to boot into MEDOS for the first time")
+
+
+
+
+def backcom():
+    global message
+    message = "Exiting GUI"
+    print("MEDOS 2.0 Beta")
+    print("Developer edition")
+    print("Copyleft () Sean Walla Walla")
+    print("========")
+terminaloop = int
+terminaloop == 1
+while terminaloop == 1:
+    print("\n")
+    consolein = input("PY://")
+    if consolein == ("help"):
+        print("Command help")
+        print("GUI - Opens the GUI interface")
+        print("PYVER - shows the python version")
+        print("Quit - ends the session")
+    if consolein == ("GUI"):
+        frame.start()
+    if consolein == ("PYVER"):
+        print("Python version: 3.6")
+    if consolein == ("Quit"):
+        terminaloop = 0
+        print("Shutting down")
+        print("Please wait")
+
+
+    # Handler to draw on canvas
+    def draw(canvas):
+        canvas.draw_text(message, [0, 20], 20, "Green")
+
+
+    # Create a frame and assign callbacks to event handlers
+    frame = simplegui.create_frame("MEDOS GUI Terminal", 480, 360)
+    frame.add_button("PY:", pyDrive)
+    frame.set_draw_handler(draw)
+    frame.add_button("Command List", commandLis)
+    frame.set_draw_handler(draw)
+    frame.add_button("Display Settings", disettings)
+    frame.set_draw_handler(draw)
+    frame.add_button("Factory reset", faresetbut)
+    frame.set_draw_handler(draw)
+    frame.add_button("Back to command line", backcom)
+    frame.set_draw_handler(draw)
+    message = "Welcome to MEDOS!"
+    frame.start()
+	'''
